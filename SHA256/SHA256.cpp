32b11c4d9e35a3dd215fc57ee632b76c6747fb4c
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2019-02-11 16:28:47 -0500

    before change to char based storage

--- SHA256/SHA256.cpp
+++ SHA256/SHA256.cpp
@@ -8,12 +8,13 @@
 #include <fstream>
 #include <string>
 
+void printBinaryRep(unsigned long long target); 
 int getFileSize(std::string fileName); 
 
 struct block
 {
 	int num_m = 0; 
-	long long m[8]; 
+	unsigned long long m[8]; 
 	block *next;
 };
 
@@ -21,12 +22,9 @@ int main(int argc, char* argv[])
 {
 	//information for padding and number of reads needed for whole file 
 	int fileSize = 0; 
-	bool needPadding = false; 
-	int numPaddingBits = 0; 
-	int numReadsNeeded = 0; 
 
 	char test; 
-	List<long long> storage;  
+	List< unsigned long long> storage;  
 	int counter = 0; 
 	block *firstBlock = nullptr; 
 	block *currentBlock = new block;
@@ -44,31 +42,14 @@ int main(int argc, char* argv[])
 			exit(1);	
 		}
 		
-		fileSize = getFileSize(argv[1]); 
-
-		/*
-		//check if the last chunk will need to be padded 
-		if ((fileSize % 64) != 0)
-		{
-			numPaddingBits = fileSize % 64;
-			numReadsNeeded = fileSize / 64;
-			needPadding = true;
-		}
-		else
-			numReadsNeeded = fileSize / 64;
-
-		//if there are less than 64 bits in the file, only need to read once
-		if (numReadsNeeded == 0) numReadsNeeded++; 
-
-		*/ 
+		fileSize = getFileSize(argv[1]); //this will be the overall length of the final message 
 
 
 		while (!myFile.eof()) //this might not be reading it quite correctly, cant tell yet it difficult to tell if the long long representation is binary accurate
-		//for (int i = 0; i < numReadsNeeded; i++)
 		{
 			//worried about pointers vs standard variables and memory storage 
-			long long temp; 
-			myFile.read(reinterpret_cast<char *> (&temp), sizeof(long long));
+			unsigned long long temp; 
+			myFile.read(reinterpret_cast<char *> (&temp), sizeof(unsigned long long));
 
 			//check if current message is full, if so - create new one
 			if (currentBlock->num_m >= 8)
@@ -88,19 +69,25 @@ int main(int argc, char* argv[])
 			}
 		}
 
+		/*
 		if (currentBlock->num_m != 8)
 		{
 			//this block is not full, need padding bits
 
 		}
+		*/
 
-		/*
-		long long v = reinterpret_cast<long long int> (firstBlock->m[0]); 
-		for (int i = 63; i >= 0; i--)
+		//check if the length of the final message is a multiple of 512 
+		if ((fileSize % 512) != 0)
 		{
-			std::cout << (v >> i) & 1)
+			//append a 1 onto the end of the message 
+			//then follow directions of padding (dont understand yet) 
+			
+			//find block that contains the last bit and travers to that block 
+			
 		}
-		*/ 
+
+		 
 
 	}
 	else
@@ -132,4 +119,15 @@ int getFileSize(std::string fileName)
 {
 	std::ifstream file(fileName, std::ios::binary | std::ios::ate);
 	return file.tellg();
+}
+
+void printBinaryRep(unsigned long long int target)
+{
+	unsigned long long tester = 1; 
+
+	for (int i = 63; i >= 0; i--)
+	{
+		std::cout << (target >> i) & 
+	}
+
 }
