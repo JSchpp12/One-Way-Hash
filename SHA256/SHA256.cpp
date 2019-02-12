0a5b7140af30b745965060bbdd09669365a9aa87
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2019-02-12 15:15:39 -0500

    Changed to SHA512 padding method --still something wrong with reading in chars at beginning

--- SHA256/SHA256.cpp
+++ SHA256/SHA256.cpp
@@ -9,20 +9,26 @@
 #include <string>
 #include <bitset>
 
-void printBinaryRep(unsigned long long target); 
-int getFileSize(std::string fileName); 
-
 struct block
 {
 	int num_m = 0; 
-	unsigned long long m[8]; 
+	unsigned long w[8]; //store message schedule for each m block? 
+	unsigned long long m[8]; //store m blocks
 	block *next;
 };
+
+void addM_Block(block *target, long long newM);
+void printBinaryRep(unsigned long long target);
+int getFileSize(std::string fileName);
+
+block* returnedBlock; //will be used to pass a block pointer back from methods 
 
 int main(int argc, char* argv[])
 {
 	//information for padding and number of reads needed for whole file 
 	int fileSize = 0; 
+
+	int messageLength = 0; 
 
 	List< unsigned long long> storage;  
 	int counter = 0; 
@@ -56,8 +62,8 @@ int main(int argc, char* argv[])
 			if (currentBlock->num_m >= 8)
 			{
 				block *previousBlock = currentBlock;
-				lastBlock = currentBlock;
 				currentBlock = new block;
+				lastBlock = currentBlock;
 				previousBlock->next = currentBlock; //set newly created block as next in chain 
 			}
 
@@ -91,10 +97,9 @@ int main(int argc, char* argv[])
 			}
 			else
 			{
-				numZeroBits = 448 - (fileSize * 8 + 1); //number of zero bits that will be needed to pad message
+				numZeroBits = 896 - (fileSize * 8 + 1); //number of zero bits that will be needed to pad message
 				std::bitset<64> currentBlock_Bit(lastBlock->m[lastBlock->num_m + 1]); 
-				
-				
+			
 				//insert last 1 into message 
 				int firstBitLoc = ((fileSize * 8) % 8);
 				currentBlock_Bit.set(firstBitLoc, 1); 
@@ -107,6 +112,15 @@ int main(int argc, char* argv[])
 					{ 
 						//input bitset into structures
 						long long bitSetBack = currentBlock_Bit.to_ullong();
+
+						if (currentBlock->num_m >= 8)
+						{
+							//current block is full need a new one 
+							block *previousBlock = currentBlock;
+							currentBlock = new block;
+							lastBlock = currentBlock;
+							previousBlock->next = currentBlock; //set newly created block as next in chain 
+						}
 						currentBlock->m[currentBlock->num_m] = bitSetBack;
 						currentBlock->num_m++;
 
@@ -117,10 +131,78 @@ int main(int argc, char* argv[])
 					firstBitLoc++; //increment counter 
 				}
 				//need to set l 
-				currentBlock->m[currentBlock->num_m] = fileSize * 8; 
+				//need to form a bitset of 128
+				//split into 2 seperate bitsets of 64 
+				//convert those into long longs and store in message blocks
+
+				std::bitset<128> sizeSet(fileSize * 8); //this will store the last bits of the padding process 
+				std::bitset<64> mostSignificant; 
+				std::bitset<64> leastSignificant; 
+				 
+				int subCounter = 0; 
+
+				//split the 128 bitset apart by testing each bit and copying output to designated sets
+				/*
+				for (int i = 127; i > 0; i--)
+				//for (int i =0; i<128; i++)
+				{
+					if (i == 64)
+						std::cout << "warning"; 
+
+					if (sizeSet.test(128 - i) == true)
+						if (i < 64)
+							mostSignificant[i - 64] = true;
+						else
+							leastSignificant[64 - i] = true;
+					else
+						if (i > 64)
+							mostSignificant[i - 64] = false;
+						else
+							leastSignificant[64 - i] = false; 
+					subCounter++; 
+				}
+				*/
+				int counter = 63; 
+
+				for (int i = 0; i < 128; i++)
+				{
+					if (sizeSet.test(i) == true)
+						if (i < 64)
+							leastSignificant[i] = true;
+						else
+							mostSignificant[i - 64] = true;
+					else
+						if (i < 64)
+							leastSignificant[i] = false;
+						else
+							mostSignificant[i - 64] = false; 
+				}
+				long long mostSigLong = mostSignificant.to_ullong(); 
+				long long leastSigLong = leastSignificant.to_ullong();
+
+				currentBlock->m[currentBlock->num_m] = mostSigLong; 
 				currentBlock->num_m++; 
+
+				//add long long to blocks 
+				addM_Block(currentBlock, mostSigLong); 
+
+				if (currentBlock != returnedBlock)
+					lastBlock = returnedBlock;
+
+				currentBlock = returnedBlock; 
+				
+				addM_Block(currentBlock, leastSigLong); 
+
+				if (currentBlock != returnedBlock)
+					lastBlock = returnedBlock;
+
+				currentBlock = returnedBlock; 
+			
+				//currentBlock->m[currentBlock->num_m] = fileSize * 8; 
+				//currentBlock->num_m++; 
 			}
 		}
+		std::cout << "complete"; 
 	}
 	else
 	{
@@ -128,6 +210,38 @@ int main(int argc, char* argv[])
 		std::cin.get(); 
 		exit(1); 
 	} 
+}
+
+void rightRotate(std::bitset<64> *currentSet, int n)
+{
+	// x - w bit word
+	//
+	// (x << n) V ( x >> w - n) 
+}
+
+void leftRotate(std::bitset<64> *currentSet, int n)
+{
+
+}
+
+//add M to target block, will return pointer to final block in the chain
+void addM_Block(block *target, long long newM)
+{
+	//check if the block has room for the new M
+	if (target->num_m < 8)
+	{
+		target->m[target->num_m] = newM; 
+		target->num_m++; 
+		returnedBlock = target; //set the return block
+	}
+	else
+	{
+		block *newBlock = new block; 
+		target->next = newBlock; 
+		newBlock->m[newBlock->num_m] = newM; 
+		newBlock->num_m++;
+		returnedBlock = newBlock; //set the return block
+	}
 }
 
 int getFileSize(std::string fileName)
